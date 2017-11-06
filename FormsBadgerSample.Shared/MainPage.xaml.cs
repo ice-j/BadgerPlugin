19bac5a1320897e89f35ef4ed4da4669964d0e70
Ice Jovanoski <ice.jovanoski@haselt.com>
2017-11-06 20:56:58 +0100

    Add Xamarin.Forms sample

---
+++ FormsBadgerSample.Shared/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Input;
+using Xam.Plugin.Badger.Abstractions;
+using Xamarin.Forms;
+
+namespace FormsSample.Shared
+{
+    public partial class MainPage : ContentPage
+    {
+        IBadgerService _badgeService;
+        public MainPage()
+        {
+            _badgeService = DependencyService.Get<IBadgerService>();
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        string badgeValue = "";
+        public string BadgeValue
+        {
+            get
+            {
+                return badgeValue;
+            }
+            set
+            {
+                var isInt = int.TryParse(value, out var parsed);
+                if (isInt)
+                    badgeValue = value;
+                OnPropertyChanged(nameof(BadgeValue));
+            }
+        }
+
+        public ICommand IncrementCommand
+            => new Command(() => _badgeService.Increment());
+
+        public ICommand DecrementCommand
+            => new Command(() => _badgeService.Decrement());
+
+        public ICommand ResetBadgeCommand
+            => new Command(() => _badgeService.Reset());
+
+        public ICommand SetBadgeCountCommand
+            => new Command<string>((count) => _badgeService.SetCount(int.Parse(count)));
+    }
+}
---
+++ FormsBadgerSample/FormsBadgerSample.Android/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xam.Plugin.Badger.Abstractions;
+using Xam.Plugin.Badger.Android;
+using FormsSample.Shared;
+using Xamarin.Forms;
+
+namespace FormsSample.Droid
+{
+    [Activity(Label = "FormsSample", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            
+            base.OnCreate(bundle);
+
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            DependencyService.Register<IBadgerService, BadgerService>();
+            DependencyService.Get<IBadgerService>().SetAndroidContext(this);
+
+            LoadApplication(new App());
+        }
+    }
+}
+
